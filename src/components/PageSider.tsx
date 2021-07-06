5a0d6e30900466365e6a354e9abcb66fb5ccd3d3
hansheng0512 <hanshengliang@outlook.com>
2021-07-07 00:54:01 +0800

    Improve Features

---
+++ src/components/PageSider.tsx
@@ -0,0 +1,71 @@
+import { Layout, Menu, Breadcrumb } from 'antd'
+import {
+    DesktopOutlined,
+    PieChartOutlined,
+    FileOutlined,
+    TeamOutlined,
+    UserOutlined,
+} from '@ant-design/icons'
+import { Outlet } from 'react-router-dom'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { LogOut } from './LogOut'
+
+const { Header, Content, Footer, Sider } = Layout
+const { SubMenu } = Menu
+
+export const PageSider = () => {
+    const [collapsed, setCollapsed] = useState(false)
+    const [isLogOutMode, setIsLogOutMode] = useState(false)
+    const navigate = useNavigate()
+
+    const onCollapse = () => {
+        setCollapsed(!collapsed)
+    }
+
+    return (
+        <Layout style={{ minHeight: '100vh' }}>
+            <Sider collapsible={true} collapsed={collapsed} onCollapse={onCollapse}>
+                <div className="logo" />
+                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                    <Menu.Item
+                        key="1"
+                        icon={<PieChartOutlined />}
+                        onClick={() => navigate('/app/insert')}
+                    >
+                        Insert Record
+                    </Menu.Item>
+                    <Menu.Item key="2" icon={<DesktopOutlined />}>
+                        Option 2
+                    </Menu.Item>
+                    <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+                        <Menu.Item key="3">Tom</Menu.Item>
+                        <Menu.Item key="4">Bill</Menu.Item>
+                        <Menu.Item key="5">Alex</Menu.Item>
+                    </SubMenu>
+                    <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+                        <Menu.Item key="6">Team 1</Menu.Item>
+                        <Menu.Item key="8">Team 2</Menu.Item>
+                    </SubMenu>
+                    <Menu.Item key="9" icon={<UserOutlined />} onClick={() => setIsLogOutMode(!isLogOutMode)}>
+                        Log Out
+                    </Menu.Item>
+                </Menu>
+            </Sider>
+            <Layout className="site-layout">
+                <Header className="site-layout-background" style={{ padding: 0 }} />
+                <Content style={{ margin: '0 16px' }}>
+                    <Breadcrumb style={{ margin: '16px 0' }}>
+                        <Breadcrumb.Item>User</Breadcrumb.Item>
+                        <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+                        {isLogOutMode && <LogOut />}
+                        <Outlet />
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+            </Layout>
+        </Layout>
+    )
+}
---
+++ src/components/Record/FormInsert.tsx
@@ -0,0 +1,75 @@
+import {
+    Form,
+    Input,
+    Button,
+    Radio,
+    Select,
+    Cascader,
+    DatePicker,
+    InputNumber,
+    TreeSelect,
+    Switch,
+} from 'antd'
+
+export const FormInsert = () => {
+    return (
+        <>
+            <Form
+                labelCol={{ span: 2 }}
+                layout={'horizontal'}
+                initialValues={{ size: 'large' }}
+                size={'large'}
+                onFinish={() => null}
+                onFinishFailed={() => null}
+            >
+                <Form.Item label="Name">
+                    <Input />
+                </Form.Item>
+                <Form.Item label="Select">
+                    <Select>
+                        <Select.Option value="demo">Demo</Select.Option>
+                    </Select>
+                </Form.Item>
+                <Form.Item label="TreeSelect">
+                    <TreeSelect
+                        treeData={[
+                            {
+                                title: 'Light',
+                                value: 'light',
+                                children: [{ title: 'Bamboo', value: 'bamboo' }],
+                            },
+                        ]}
+                    />
+                </Form.Item>
+                <Form.Item label="Cascader">
+                    <Cascader
+                        options={[
+                            {
+                                value: 'zhejiang',
+                                label: 'Zhejiang',
+                                children: [
+                                    {
+                                        value: 'hangzhou',
+                                        label: 'Hangzhou',
+                                    },
+                                ],
+                            },
+                        ]}
+                    />
+                </Form.Item>
+                <Form.Item label="DatePicker">
+                    <DatePicker />
+                </Form.Item>
+                <Form.Item label="InputNumber">
+                    <InputNumber />
+                </Form.Item>
+                <Form.Item label="Switch">
+                    <Switch />
+                </Form.Item>
+                <Form.Item label="Button">
+                    <Button>Button</Button>
+                </Form.Item>
+            </Form>
+        </>
+    )
+}
